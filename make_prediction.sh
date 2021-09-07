ea78af7258268b31dcaa9bdfa258d51dd7ad59b8
Zian Jiang <zj444@nyu.edu>
2021-09-07 19:13:51 -0400

    Update make_prediction.sh

--- make_prediction.sh
+++ make_prediction.sh
@@ -5,15 +5,9 @@ echo "Port: $PORT"
 
 # POST method predict
 curl -d '{  
-   "EXPERIENCE":{  
-      "0":5
-   },
-   "TEST":{  
-      "0":5
-   },
-   "INTERVIEW":{  
-      "0":5
-   }
+   "EXPERIENCE": 5,
+   "TEST": 5,
+   "INTERVIEW": 5
 }'\
      -H "Content-Type: application/json" \
      -X POST http://localhost:$PORT/predict_api
