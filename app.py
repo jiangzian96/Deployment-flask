671c68c9f75ba4f2db7f88576cd104ba20cb9f04
Zian Jiang <zj444@nyu.edu>
2021-09-07 18:48:45 -0400

    Update app.py

--- app.py
+++ app.py
@@ -13,9 +13,9 @@ def home():
 '''
 @app.route('/predict',methods=['POST'])
 def predict():
-    '''
+    
     For rendering results on HTML GUI
-    '''
+    
     int_features = [int(x) for x in request.form.values()]
     final_features = [np.array(int_features)]
     prediction = model.predict(final_features)
