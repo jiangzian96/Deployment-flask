dd47bb4d20be9887e4d11d70d38641900d0b8e66
EC2 Default User <ec2-user@ip-172-31-39-121.us-east-2.compute.internal>
2021-09-07 18:19:09 +0000

    docker

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM python:3.7.3-stretch
+
+WORKDIR /app
+
+COPY . /app/
+
+RUN pip install --upgrade pip &&\
+    pip install --trusted-host pypi.python.org -r requirements.txt
+    
+EXPOSE 80
--- request.py
+++ request.py
@@ -8,7 +8,7 @@ parser.add_argument("--test", type=int, required=True)
 args = parser.parse_args()
 
 
-url = 'http://127.0.0.1:8080/predict_api'
+url = 'http://localhost:80/predict_api'
 r = requests.post(url,json={'experience':args.experience, 'test_score':args.test, 'interview_score': args.interview})
 
 print(r.json())
