49051b0e603c448c621001366ac8e8e8d64e79cd
Zian Jiang <zj444@nyu.edu>
2021-09-08 16:20:54 -0400

    Create make_prediction_aws.sh

---
+++ make_prediction_aws.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+PORT=8888
+echo "Port: $PORT"
+
+# POST method predict
+curl -d '{  
+   "EXPERIENCE": 5,
+   "TEST": 5,
+   "INTERVIEW": 5
+}'\
+     -H "Content-Type: application/json" \
+     -X POST http://http://ec2-3-142-230-226.us-east-2.compute.amazonaws.com:$PORT/predict_api
